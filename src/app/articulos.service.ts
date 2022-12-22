a5693a6bf58c0947a365b4cbd2c13d92281056ed
David Malpica <dmalpica96@hotmail.com>
2022-12-21 21:37:24 -0600

    Add http client

--- src/app/articulos.service.ts
+++ src/app/articulos.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticulosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  retornar() {
+    return this.http.get("https://scratchya.com.ar/vue/datos.php");
+  }
 }
