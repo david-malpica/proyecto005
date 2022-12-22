3ed1858f8b789f33ab6d120b83ef7821b46a6c66
David Malpica <dmalpica96@hotmail.com>
2022-12-21 21:41:52 -0600

    Add articulo service in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ArticulosService } from './articulos.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto005';
+  articulos:any;
+  constructor(private articuloServicio: ArticulosService){}
+  ngOnInit(){
+    this.articuloServicio.retornar()
+    .subscribe(result=> this.articulos=result);
+  }
 }
